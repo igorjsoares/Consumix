c5240b0f07e8a4b7cb18ca2d2d9132e2ae887965
Igor Soares <igorjsoares@gmail.com>
2019-12-01 16:22:31 +0000

    Ainda no toatr 13

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ include 'head.php';
         
 		console.log('Teste de console.');
         
-        toastr.error('Algo errado com o e-mail ou senha.', 'Ops!!!', {
+        toastr.info('Algo errado com o e-mail ou senha.', 'Ops!!!', {
             
             positionClass: "toast-top-center",
             preventDuplicates: true,
