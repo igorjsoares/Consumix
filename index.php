38b637dcf21852a488c0ffa7d5b626ffc55be2e8
Igor Soares <igorjsoares@gmail.com>
2019-12-01 16:11:09 +0000

    Ainda no toatr 6

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ include 'head.php';
         
 		console.log('Teste de console.');
         
-        toastr.error('Algo errado com o e-mail ou senha.', 'Ops!!!', {timeOut: 3000})
+        toastr.error('Algo errado com o e-mail ou senha.', 'Ops!!!', {timeOut: 3000, positionClass: 'toast-top-center'})
 	</script>
 
 	<?php
