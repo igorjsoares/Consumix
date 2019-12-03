de125cb1b82f25f52e4a3ac6f914051d2237f401
Igor Soares <igorjsoares@gmail.com>
2019-12-03 00:03:51 +0000

    Verificando se o Umble funciona 3

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ include 'head.php';
 
 	<div class="login-box">
 		<div class="login-logo">
-			<a href="../../index2.html"><b>CONSU</b>mix</a>
+			<a href="../../index2.html"><b>consu</b>MIX</a>
 		</div>
 		<!-- /.login-logo -->
 		<div class="card">
