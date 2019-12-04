af912f871b96b3b579a6bfbe39e2fe8604a3c2f6
Igor Soares <igorjsoares@gmail.com>
2019-12-04 15:22:29 +0000

    Saúde 15

--- saude.php
+++ saude.php
@@ -2,11 +2,12 @@
 require_once 'vendor/autoload.php';
 
 $cansaco = $_GET['cansaco'];
-$usuario->setCansaco($cansaco);
+
 
 $usuario = new \App\Model\Usuario();
-$usuariosDao = new \App\Model\usuariosDao();
+$usuario->setCansaco($cansaco);
 
+$usuariosDao = new \App\Model\usuariosDao();
 $resultado = $usuariosDao->criarCansaco($usuario);
 
 var_dump($resultado);
